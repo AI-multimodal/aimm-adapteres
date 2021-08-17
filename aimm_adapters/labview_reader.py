83a36c437491fd3759cb3b7d88ec74983c630ee0
jmaruland <jmaruland@bnl.gov>
2021-08-17 08:48:02 -0700

    Change: case values for enum class

--- aimm_adapters/labview_reader.py
+++ aimm_adapters/labview_reader.py
@@ -1,4 +1,19 @@
+from enum import Enum
 import pandas as pd
+
+
+class ParsingCase(Enum):
+    column = 1
+    user = 2
+    scan = 3
+    amplifier = 4
+    analog = 5
+    mono = 6
+    id_info = 7
+    slit = 8
+    motor = 9
+    panel = 10
+    beamline = 11
 
 
 class LabviewFileReader:
@@ -20,7 +35,9 @@ class LabviewFileReader:
             # Parse comments as metadata
             if line[0] == "#":
                 if len(line) > 2:
-                    if parsing_case == 1:
+                    # The next line after the Column Headinds tag is the only line
+                    # that does not include a white space after the comment/hash symbol
+                    if parsing_case == ParsingCase.column:
                         line = line[1:]
                     else:
                         line = line[2:]
@@ -30,73 +47,76 @@ class LabviewFileReader:
                     # Start reading the name of the upcoming block of information
                     comment_lines = []
                     if line == "Column Headings:":
-                        parsing_case = 1  # Create headers for dataframe
+                        parsing_case = (
+                            ParsingCase.column
+                        )  # Create headers for dataframe
                         continue
                     elif line == "User Comment:":
-                        parsing_case = 2
+                        parsing_case = ParsingCase.user
                         continue
                     elif line == "Scan config:":
-                        parsing_case = 3
+                        parsing_case = ParsingCase.scan
                         continue
                     elif line == "Amplifier Sensitivities:":
-                        parsing_case = 4
+                        parsing_case = ParsingCase.amplifier
                         continue
                     elif line.find("Analog Input Voltages") != -1:
-                        parsing_case = 5
+                        parsing_case = ParsingCase.analog
                         continue
                     elif line == "Mono Info:":
-                        parsing_case = 6
+                        parsing_case = ParsingCase.mono
                         continue
                     elif line == "ID Info:":
-                        parsing_case = 7
+                        parsing_case = ParsingCase.id_info
                         continue
                     elif line == "Slit Info:":
-                        parsing_case = 8
+                        parsing_case = ParsingCase.slit
                         continue
                     elif line == "Motor Positions:":
-                        parsing_case = 9
+                        parsing_case = ParsingCase.motor
                         continue
                     elif line.find("LabVIEW Control Panel") != -1:
-                        parsing_case = 10
+                        parsing_case = ParsingCase.panel
                     elif line.find("Beamline") != -1:
-                        parsing_case = 11
-                    # Reads the following lines for a specific block of information
-                    if parsing_case == 1:
+                        parsing_case = ParsingCase.beamline
+                    # Reads the following lines to parse a block of information
+                    # with a specific format
+                    if parsing_case == ParsingCase.column:
                         line = line.replace("XMAP12:DT Corr I0", "XMAP12:DT_Corr_I0")
                         line = " ".join(line.split())  # Remove unwanted white spaces
                         headers = line.split()
                         meta_dict["Columns"] = headers
                         parsing_case = 0
-                    elif parsing_case == 2:
+                    elif parsing_case == ParsingCase.user:
                         line = " ".join(line.split())  # Remove unwanted white spaces
                         comment_lines.append(line)
                         meta_dict["UserComment"] = comment_lines
-                    elif parsing_case == 3:
+                    elif parsing_case == ParsingCase.scan:
                         line = " ".join(line.split())  # Remove unwanted white spaces
                         comment_lines.append(line)
                         meta_dict["ScanConfig"] = comment_lines
-                    elif parsing_case == 4:
+                    elif parsing_case == ParsingCase.amplifier:
                         comment_lines = line.split("  ")
                         meta_dict["AmplifierSensitivities"] = comment_lines
-                    elif parsing_case == 5:
+                    elif parsing_case == ParsingCase.analog:
                         comment_lines = line.split("  ")
                         meta_dict["AnalogInputVoltages"] = comment_lines
-                    elif parsing_case == 6:
+                    elif parsing_case == ParsingCase.mono:
                         comment_lines = line.split("; ")
                         meta_dict["MonoInfo"] = comment_lines
-                    elif parsing_case == 7:
+                    elif parsing_case == ParsingCase.id_info:
                         comment_lines = line.split("  ")
                         meta_dict["IDInfo"] = comment_lines
-                    elif parsing_case == 8:
+                    elif parsing_case == ParsingCase.slit:
                         comment_lines = line.split("   ")
                         meta_dict["SlitInfo"] = comment_lines
-                    elif parsing_case == 9:
+                    elif parsing_case == ParsingCase.motor:
                         comment_lines = line.split("  ")
                         meta_dict["MotorPositions"] = comment_lines
-                    elif parsing_case == 10:
+                    elif parsing_case == ParsingCase.panel:
                         comment_lines = line.split("; ")
                         meta_dict["File"] = comment_lines
-                    elif parsing_case == 11:
+                    elif parsing_case == ParsingCase.beamline:
                         meta_dict["Beamline"] = line
                 else:
                     parsing_case = 0
