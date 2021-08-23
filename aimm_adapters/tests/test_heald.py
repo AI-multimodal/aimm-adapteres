7a03bbb9ca236c64282e20af75d3d5aa1e8bbe7c
jmaruland <jmaruland@bnl.gov>
2021-08-23 13:36:16 -0700

    whitespace

--- aimm_adapters/tests/test_heald.py
+++ aimm_adapters/tests/test_heald.py
@@ -8,16 +8,13 @@ from ..heald_labview import HealdLabViewTree
 from tiled.client import from_tree
 from tiled.trees.in_memory import Tree
 
+
 @pytest.mark.parametrize(
     "directory, filename, expected_size",
-    [
-     ("../files/", "test_data.01", (2, 4))
-     ],
-    )
+    [("../files/", "test_data.01", (2, 4))],
+)
 def test_heald_labview(directory, filename, expected_size):
     tree = Tree({"A": HealdLabViewTree.from_directory(directory)})
-    client = from_tree(tree)    
+    client = from_tree(tree)
     arr = client["A"][filename].read()
     assert arr.shape == expected_size
-    
-    
