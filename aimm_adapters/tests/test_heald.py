e4c1a5ad8924312ed97beb59a919fa071169bc8e
jmaruland <jmaruland@bnl.gov>
2021-08-24 07:11:29 -0700

    changed path for test

--- aimm_adapters/tests/test_heald.py
+++ aimm_adapters/tests/test_heald.py
@@ -1,17 +1,19 @@
 import pytest
 
-
+from pathlib import Path
 from ..heald_labview import HealdLabViewTree
 from tiled.client import from_tree
 from tiled.trees.in_memory import Tree
 
 
 @pytest.mark.parametrize(
-    "directory, filename, expected_size",
-    [("../files/", "test_data.01", (2, 4))],
+    "filename, expected_size",
+    [("test_data.01", (2, 4))],
 )
-def test_heald_labview(directory, filename, expected_size):
-    tree = Tree({"A": HealdLabViewTree.from_directory(directory)})
+def test_heald_labview(filename, expected_size):
+    here = Path(__file__).parent  # directory containing this module
+    files = here / ".." / "files"
+    tree = Tree({"A": HealdLabViewTree.from_directory(files)})
     client = from_tree(tree)
     arr = client["A"][filename].read()
     assert arr.shape == expected_size
